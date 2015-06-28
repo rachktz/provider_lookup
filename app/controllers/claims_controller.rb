c19ea5486db38aa8399415d6c570190ee7b2cfb1
Rachel Katz <rachelleorakatz@gmail.com>
2015-06-27 23:14:15 -0700

    started avg calculations

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -6,6 +6,7 @@ class ClaimsController < ApplicationController
 	def index
 		@npi = params[:npi]
 		@claims = Claim.find_by_npi @npi
+		@claims_sort = @claims.sort_by { |h| h["line_srvc_cnt"].to_i }.reverse
 	end
 
 end
--- app/models/claim.rb
+++ app/models/claim.rb
@@ -8,4 +8,12 @@ class Claim
 	def self.find_by_npi npi
 		get('/resource/5fnr-qp4c.json', :query => {:npi => npi})
 	end
+
+	def self.find_by_hcpcs_code hcpcs_code
+		get('/resource/5fnr-qp4c.json', :query => {:hcpcs_code => hcpcs_code})
+	end
+
 end
+
+claims = Claim.find_by_hcpcs_code "55700"
+puts claims
