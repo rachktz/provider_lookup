a524f6997a515f959f79d8f255e5cd0227f3dd6d
Rachel Katz <rachelleorakatz@gmail.com>
2015-06-28 17:07:02 -0700

    added avg_charge and avg_payment

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -9,11 +9,14 @@ class Claim
 		get('/resource/5fnr-qp4c.json', :query => {:npi => npi})
 	end
 
-	def self.find_by_hcpcs_code hcpcs_code
-		get('/resource/5fnr-qp4c.json', :query => {:hcpcs_code => hcpcs_code})
+	def self.avg_charge (procedure_code, place)
+		resource = get('/resource/5fnr-qp4c.json', query: {hcpcs_code: procedure_code, place_of_service: place})
+		@avg_charge = (resource.inject(0) { |result, x| result + x["average_submitted_chrg_amt"].to_f })/resource.length
 	end
 
+	def self.avg_payment (procedure_code, place)
+		resource = get('/resource/5fnr-qp4c.json', query: {hcpcs_code: procedure_code, place_of_service: place})
+		@avg_payment = (resource.inject(0) { |result, x| result + x["average_medicare_payment_amt"].to_f })/resource.length
+	end
 end
 
-claims = Claim.find_by_hcpcs_code "55700"
-puts claims
